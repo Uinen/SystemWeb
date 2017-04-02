a1a5bfbdc7dccc58dfd2a17710448b1c8a5a1b15
Valentino <vale92graveglia@live.it>
2017-04-02 14:52:36 +0200

    SystemWeb 3.0.4 - Update#03 Added UsersImageRepository

---
+++ Repository/Interface/iUsersImageRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using SystemWeb.Models;
+
+namespace SystemWeb.Repository.Interface
+{
+    public interface iUsersImageRepository
+    {
+        /// <summary>
+        /// GET ALL UsersImages
+        /// </summary>
+        /// <returns>It return all UsersImages placed in the Database</returns>
+        IEnumerable<UsersImage> GetUsersImages();
+
+        /// <summary>
+        /// GET UsersImages BY ID
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns>It return the UsersImages by given Id</returns>
+        UsersImage GetUsersImagesById(Guid? Id);
+
+        /// <summary>
+        /// Insert specified UsersImages
+        /// </summary>
+        /// <param name="insertUsersImages"></param>
+        void InsertUsersImages(UsersImage insertUsersImages);
+
+        /// <summary>
+        /// Update specified UsersImages
+        /// </summary>
+        /// <param name="updateUsersImages"></param>
+        void UpdateUsersImages(UsersImage updateUsersImages);
+
+        /// <summary>
+        /// Delete specified pv
+        /// </summary>
+        /// <param name="Id"></param>
+        void DeleteUsersImages(Guid? Id);
+
+        /// <summary>
+        /// Save method
+        /// </summary>
+        void Save();
+    }
+}
---
+++ Repository/UsersImageRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SystemWeb.Models;
+using SystemWeb.Repository.Interface;
+
+namespace SystemWeb.Repository
+{
+    public class UsersImageRepository : iUsersImageRepository
+    {
+        public void DeleteUsersImages(Guid? Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<UsersImage> GetUsersImages()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UsersImage GetUsersImagesById(Guid? Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertUsersImages(UsersImage insertUsersImages)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateUsersImages(UsersImage updateUsersImages)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
